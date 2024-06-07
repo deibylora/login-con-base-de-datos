edeb5c0288b27b54a7ec9746edc15108374a5c3f
deiby <thefrogzes@gmail.com>
2024-06-07 09:58:21 -0400

    actualizado1

---
+++ login.py
@@ -0,0 +1,29 @@
+from modulologin import registro, login
+
+print ("Bienvenido a Facebook\n")
+
+while True:
+
+    print("\n---Menu principal---\n")
+
+    menu = input("Elija una opcion: 1.Registrarse 2.Iniciar sesion 3.Salir\n")
+    
+    if menu in ('1', '2', '3'):
+        if menu == '1':
+            usuario_registro = input("Registre su usuario: ")
+            contraseña_registro =input("Registre su contrasena: ")
+            registro(usuario_registro, contraseña_registro)
+            print (registro)
+            
+        elif menu == '2':
+            usuario_entrada = input("Ingrese su usuario: ")
+            contraseña_entrada =input("Ingrese su contrasena: ")
+            login(usuario_entrada, contraseña_entrada)
+            print(login)
+
+        elif menu == '3':
+            print("Saliendo...")
+            break
+        
+        else:
+            print("Elije una opcion valida")
---
+++ modulologin.py
@@ -0,0 +1,23 @@
+
+def login (user, password):
+    with open ("Base_datos.txt", "r") as datos:
+        for line in datos:
+            stored_user, stored_password = line.strip().split()
+            if user == stored_user and password == stored_password:
+                return f"Bienvenido: {user}"
+        if user != stored_user and password != stored_password:
+            return F"Usuario o contraseña incorrecta, intente nuevamente"
+            
+def registro (user, password):
+    with open ("Base_datos.txt", "a+") as datos:
+        for line in datos:
+            stored_user, _ = line.strip().split()
+            if user == stored_user:
+                return ("Usuario ya existe, intente otro")
+        datos.write(f"{user},{password}\n")
+        return f"Gracias por registrarse {user}"
+
+    
+
+         
+
